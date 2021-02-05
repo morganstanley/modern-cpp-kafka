59a39224cff5d36ad92fde7298593d53c89943a5
Yinxiu Jia <48558845+kenneth-jia@users.noreply.github.com>
2021-02-05 16:26:14 +0800

    Add some examples (#11)
    * Add some examples

---
+++ examples/kafka_async_producer_not_copy_payload.cc
@@ -0,0 +1,61 @@
+#include "kafka/KafkaProducer.h"
+
+#include <csignal>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <brokers> <topic>\n";
+        exit(1);
+    }
+
+    std::string brokers = argv[1];
+    kafka::Topic topic  = argv[2];
+
+    try {
+
+        // Create configuration object
+        kafka::Properties props ({
+            {"bootstrap.servers",  brokers},
+            {"enable.idempotence", "true"},
+        });
+
+        // Create a producer instance.
+        kafka::KafkaAsyncProducer producer(props);
+
+        // Read messages from stdin and produce to the broker.
+        std::cout << "% Type message value and hit enter to produce message. (empty line to quit)" << std::endl;
+
+        for (auto line = std::make_shared<std::string>();
+             std::getline(std::cin, *line);
+             line = std::make_shared<std::string>()) {
+            // The ProducerRecord doesn't own `line`, it is just a thin wrapper
+            auto record = kafka::ProducerRecord(topic,
+                                                kafka::Key(),
+                                                kafka::Value(line->c_str(), line->size()));
+
+            // Send the message.
+            producer.send(record,
+                          // Note: Here we capture the shared_pointer of `line`,
+                          //       which holds the content for `record.value()`.
+                          //       It makes sure the memory block is valid until the lambda finishes.
+                          [line](const kafka::Producer::RecordMetadata& metadata, std::error_code ec) {
+                              if (!ec) {
+                                  std::cout << "% Message delivered: " << metadata.toString() << std::endl;
+                              } else {
+                                  std::cerr << "% Message delivery failed: " << ec.message() << std::endl;
+                              }
+                          });
+
+            if (line->empty()) break;
+        }
+
+        // producer.close(); // No explicit close is needed, RAII will take care of it
+
+    } catch (const kafka::KafkaException& e) {
+        std::cerr << "% Unexpected exception caught: " << e.what() << std::endl;
+    }
+}
+
---
+++ examples/kafka_manual_commit_consumer.cc
@@ -0,0 +1,71 @@
+#include "kafka/KafkaConsumer.h"
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <brokers> <topic>\n";
+        exit(1);
+    }
+
+    std::string brokers = argv[1];
+    kafka::Topic topic  = argv[2];
+
+    try {
+
+        // Create configuration object
+        kafka::Properties props ({
+            {"bootstrap.servers", brokers},
+        });
+
+        // Create a consumer instance.
+        kafka::KafkaManualCommitConsumer consumer(props);
+
+        // Subscribe to topics
+        consumer.subscribe({topic});
+
+        auto lastTimeCommitted = std::chrono::steady_clock::now();
+
+        // Read messages from the topic.
+        std::cout << "% Reading messages from topic: " << topic << std::endl;
+        while (true) {
+            auto records = consumer.poll(std::chrono::milliseconds(100));
+            for (const auto& record: records) {
+                // In this example, quit on empty message
+                if (record.value().size() == 0) return 0;
+
+                if (!record.error()) {
+                    std::cout << "% Got a new message..." << std::endl;
+                    std::cout << "    Topic    : " << record.topic() << std::endl;
+                    std::cout << "    Partition: " << record.partition() << std::endl;
+                    std::cout << "    Offset   : " << record.offset() << std::endl;
+                    std::cout << "    Timestamp: " << record.timestamp().toString() << std::endl;
+                    std::cout << "    Headers  : " << kafka::toString(record.headers()) << std::endl;
+                    std::cout << "    Key   [" << record.key().toString() << "]" << std::endl;
+                    std::cout << "    Value [" << record.value().toString() << "]" << std::endl;
+                } else {
+                    std::cerr << record.toString() << std::endl;
+                }
+            }
+
+            if (!records.empty()) {
+                auto now = std::chrono::steady_clock::now();
+                if (now - lastTimeCommitted > std::chrono::seconds(1)) {
+                    // Commit offsets for messages polled
+                    std::cout << "% syncCommit offsets: " << kafka::Utility::getCurrentTime() << std::endl;
+                    consumer.commitSync(); // or commitAsync()
+
+                    lastTimeCommitted = now;
+                }
+            }
+        }
+
+        // consumer.close(); // No explicit close is needed, RAII will take care of it
+
+    } catch (const kafka::KafkaException& e) {
+        std::cerr << "% Unexpected exception caught: " << e.what() << std::endl;
+    }
+}
+
