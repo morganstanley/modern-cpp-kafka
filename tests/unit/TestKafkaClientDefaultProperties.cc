9529a141494277423b3469eb512af229e79b616e
Gobind Prasad <gobprasad@gmail.com>
2021-05-21 02:05:05 +0530

    fixed merge conflicts

--- tests/unit/TestKafkaClientDefaultProperties.cc
+++ tests/unit/TestKafkaClientDefaultProperties.cc
@@ -1,3 +1,5 @@
+#include "../utils/TestUtility.h"
+
 #include "kafka/ConsumerConfig.h"
 #include "kafka/KafkaConsumer.h"
 #include "kafka/KafkaProducer.h"
@@ -65,7 +67,7 @@ TEST(KafkaClient, KafkaProducerDefaultProperties)
         EXPECT_TRUE(checkProperties("KafkaAsyncProducer", producer, expectedKVs));
     }
 
-    std::cout << "--------------------" << std::endl;
+    KafkaTestUtility::PrintDividingLine();
 
     {
         auto props = commonProps.put(Kafka::ProducerConfig::ENABLE_IDEMPOTENCE, "true");
@@ -81,7 +83,7 @@ TEST(KafkaClient, KafkaProducerDefaultProperties)
         EXPECT_TRUE(checkProperties("KafkaAsyncProducer(idempotence enabled)", producer, expectedKVs));
     }
 
-    std::cout << "--------------------" << std::endl;
+    KafkaTestUtility::PrintDividingLine();
 
     {
         Kafka::KafkaSyncProducer producer(commonProps);
@@ -107,7 +109,7 @@ TEST(KafkaClient, KafkaConsumerDefaultProperties)
             { Kafka::ConsumerConfig::ENABLE_PARTITION_EOF,  "false"      },
             { Kafka::ConsumerConfig::MAX_POLL_RECORDS,      "500"        },
             { Kafka::ConsumerConfig::QUEUED_MIN_MESSAGES,   "100000"     },
-            { Kafka::ConsumerConfig::SESSION_TIMEOUT_MS,    "10000"      },
+            { Kafka::ConsumerConfig::SESSION_TIMEOUT_MS,    "45000"      },
             { Kafka::ConsumerConfig::SOCKET_TIMEOUT_MS,     "60000"      },
             { Kafka::ConsumerConfig::SECURITY_PROTOCOL,     "plaintext"  },
             { "enable.auto.commit",                         "false"      },
@@ -121,7 +123,7 @@ TEST(KafkaClient, KafkaConsumerDefaultProperties)
         EXPECT_FALSE(consumer.getProperty(Kafka::ConsumerConfig::AUTO_OFFSET_RESET));
     }
 
-    std::cout << "--------------------" << std::endl;
+    KafkaTestUtility::PrintDividingLine();
 
     {
         Kafka::KafkaManualCommitConsumer consumer(commonProps);
--- tests/utils/TestUtility.h
+++ tests/utils/TestUtility.h
@@ -60,6 +60,11 @@
 namespace Kafka = KAFKA_API;
 
 namespace KafkaTestUtility {
+inline void
+PrintDividingLine(const std::string& description = "")
+{
+    std::cout << "---------------" << description << "---------------" << std::endl;
+}
 
 inline Kafka::Properties
 GetKafkaClientCommonConfig()
@@ -172,9 +177,10 @@ CreateKafkaTopic(const Kafka::Topic& topic, int numPartitions, int replicationFa
 {
     Kafka::AdminClient adminClient(GetKafkaClientCommonConfig());
     auto createResult = adminClient.createTopics({topic}, numPartitions, replicationFactor);
+    std::cout << "[" << Kafka::Utility::getCurrentTime() << "] " << __FUNCTION__ << ": create topic[" << topic << "] "
+       << "with numPartitions[" << numPartitions << "], replicationFactor[" << replicationFactor << "]. Result: " << createResult.message() << std::endl;
     ASSERT_FALSE(createResult.errorCode());
-    std::cout << "[" << Kafka::Utility::getCurrentTime() << "] " << __FUNCTION__ << ": topic[" << topic << "] created with numPartitions[" << numPartitions << "], replicationFactor[" << replicationFactor << "]." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
 class JoiningThread {
@@ -220,20 +226,23 @@ signalToAllBrokers(int sig)
     else if (sig == SIGCONT)
     {
         std::cout << "[" << Kafka::Utility::getCurrentTime() << "] Brokers resumed"  << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
 
 inline void
 PauseBrokers()
 {
+    constexpr int WAIT_AFTER_PAUSE_MS = 100;
     signalToAllBrokers(SIGSTOP);
+    std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_AFTER_PAUSE_MS));
 }
 
 inline void
 ResumeBrokers()
 {
+    constexpr int WAIT_AFTER_RESUME_SEC = 5;
     signalToAllBrokers(SIGCONT);
+    std::this_thread::sleep_for(std::chrono::seconds(WAIT_AFTER_RESUME_SEC));
 }
 
 inline std::shared_ptr<JoiningThread>
